24d9ee67bb5e43e8756fdaa4056f8077dd5fbcb1
Pixel764 <zepterv26@gmail.com>
2022-12-04 21:36:03 +0200

    Search bar

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-	path('', views.HomepageView.as_view(), name='homepage')
+	path('', views.HomepageView.as_view(), name='homepage'),
+	path('search/', views.SearchView.as_view(), name='search_posts')
 ]
--- main/views.py
+++ main/views.py
@@ -11,3 +11,15 @@ class HomepageView(ListView):
 
 	def get_queryset(self):
 		return Post.objects.filter(published_date__lte=timezone.now()).values('title', 'pk')
+
+
+class SearchView(ListView):
+	model = Post
+	template_name = 'index.html'
+	context_object_name = 'posts'
+	paginate_by = 30
+
+	def get_queryset(self):
+		return Post.objects.filter(
+			title__icontains=self.request.GET['q'], published_date__lte=timezone.now()
+		).values('title', 'pk')
